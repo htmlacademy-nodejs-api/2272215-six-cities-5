21fbfe3dfcdfe47c7e06dd25f5e520d0e68c0d91
Lenara K <lkrocket800@gmail.com>
2023-10-11 19:24:37 +0300

    chore: add salt and db host to env config

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -5,6 +5,8 @@ convict.addFormats(validator);
 
 export type RestSchema = {
   PORT: number;
+  SALT: string;
+  MONGO_DB: string;
 }
 
 export const validateRestSchema = convict<RestSchema>({
@@ -13,5 +15,17 @@ export const validateRestSchema = convict<RestSchema>({
     format: 'port',
     env: 'PORT',
     default: 4000
+  },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: '',
+  },
+  MONGO_DB: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'MONGO_DB',
+    default: '127.0.0.1'
   },
 });
