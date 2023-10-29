1e16e28cbc448675d5c7e09199367eb26301da8d
Lenara K <lkrocket800@gmail.com>
2023-10-29 16:28:04 +0300

    feat(rest): create user entiry and user model

---
+++ src/shared/modules/user/user-entity.ts
@@ -0,0 +1,49 @@
+import type { Types } from 'mongoose';
+import { getModelForClass, defaultClasses, modelOptions, prop } from '@typegoose/typegoose';
+import { User } from '../../types/index.js';
+import { createSHA256 } from '../../utils/index.js';
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+  }
+})
+export class UserEntity extends defaultClasses.TimeStamps implements User {
+  _id: Types.ObjectId;
+
+  id: string;
+
+  @prop({ unique: true, required: true })
+  public email: string;
+
+  @prop({ required: false, default: '' })
+  public avatarPath: string;
+
+  @prop({ required: true, default: '' })
+  public firstName: string;
+
+  @prop({ required: true, default: '' })
+  public lastName: string;
+
+  @prop({ require: true, default: '' })
+  private passwordHash?: string;
+
+  constructor(userData: User) {
+    super();
+
+    this.email = userData.email;
+    this.avatarPath = userData.avatarPath;
+    this.firstName = userData.firstName;
+    this.lastName = userData.lastName;
+  }
+
+  public setPasswordHash(password: string, salt: string) {
+    this.passwordHash = createSHA256(password, salt);
+  }
+
+  public getPasswordHash(): string | undefined {
+    return this.passwordHash;
+  }
+}
+
+export const UserModel = getModelForClass(UserEntity);
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,3 +1,4 @@
 export { getErrorMessage } from './get-error-message.js';
 export * from './get-random-values/index.js';
 export * from './database/index.js';
+export { createSHA256 } from './create-sha-256.js';
