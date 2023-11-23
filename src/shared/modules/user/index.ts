a9a168e0b770d83fdcfd5e0f9cf226930b19c112
Lenara K <lkrocket800@gmail.com>
2023-11-23 23:21:54 +0300

    feat: implement create method in user controller

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -1,5 +1,6 @@
 export * from './types.js';
 export { UserEntity, userModel } from './user-entity.js';
 export * from './user-dto.js';
+export * from './user-rdo.js';
 export { UserService } from './user-service.js';
 export * from './utils/index.js';
--- src/shared/modules/user/user-controller.ts
+++ src/shared/modules/user/user-controller.ts
@@ -1,14 +1,19 @@
 import { NextFunction, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import { inject, injectable } from 'inversify';
-import { ILogger } from '../../libs/index.js';
+import { ILogger, IConfig, RestSchema } from '../../libs/index.js';
 import { Component } from '../../types/index.js';
-import { BaseController, HttpMethod } from '../../libs/rest/index.js';
-import { CreateUserRequest } from './types.js';
+import { BaseController, HttpError, HttpMethod } from '../../libs/rest/index.js';
+import { fillDTO } from '../../utils/index.js';
+import { CreateUserRequest, IUserService } from './types.js';
+import { UserRdo } from './user-rdo.js';
 
 @injectable()
 export class UserController extends BaseController {
   constructor(
     @inject(Component.Logger) protected readonly logger: ILogger,
+    @inject(Component.UserService) private readonly userService: IUserService,
+    @inject(Component.Config) private readonly configService: IConfig<RestSchema>,
   ) {
     super(logger);
 
@@ -17,10 +22,19 @@ export class UserController extends BaseController {
   }
 
   public async create (
-    _req: CreateUserRequest,
-    _res: Response,
+    req: CreateUserRequest,
+    res: Response,
     _next: NextFunction,
   ): Promise<void> {
-    throw new Error('UserController, Throw Error');
+    const { body } = req;
+    const foundUser = await this.userService.findByEmail(body.email);
+
+    if(foundUser) {
+      throw new HttpError(StatusCodes.CONFLICT, 'Cannot create user with such email');
+    }
+
+    const newUser = await this.userService.create(body, this.configService.get('SALT'));
+    const userRdo = fillDTO(UserRdo, newUser);
+    this.created(res, userRdo);
   }
 }
